ae6276258480ef9fc77941192b90d3e3cfa14893
DhanyG <133573056+DhanGaringan-CPE@users.noreply.github.com>
2023-11-29 10:35:36 +0000

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,20 +1,35 @@
-from flask 
-import Flask, jsonify, request 
+from flask import Flask, jsonify, request 
 app = Flask(__name__) 
-hearts =    [{"heart_id": "h1", "date": "11/15/2023", "heart_rate": "120bpm"},     
-            {"heart_id": "h2", "date": "11/17/2023", "heart_rate": "170bpm"},] 
-@app.route("/hearts", methods=['GET']) 
 
-def getHearts():     
-    return jsonify(hearts) 
+hearts = [
+    {
+        "heart_id":"heart_1",
+        "date": "29/11/23",
+        "heart_rate":"120bpm"
+    },
+    {
+        "heart_id":"heart_2",
+        "date": "29/11/23",
+        "heart_rate":"110bpm"
+    }
+]
+
+@app.route("/hearts", methods = ['Get'])
+def getHearts():
+    return jsonify(hearts)
+    
 @app.route("/hearts", methods=['POST']) 
 def add_hearts():     
     heart = request.get_json()     
-    hearts.append(heart)     
+    hearts.append(hearts)     
     return {'id': len(hearts)}, 200 
 @app.route('/hearts/<int:index>', methods=['DELETE']) 
 def delete_heart(index):     
     hearts.pop(index)     
     return 'The heart rate has been deleted', 200 
+    
+    
+    
+    
 if __name__ == "__main__":     
     app.run()
